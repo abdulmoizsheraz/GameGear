609a0371302ffc13738328135695598872d5b161
MoizSheraz <100522059+moizsheraz@users.noreply.github.com>
2023-05-04 20:24:47 +0500

    RemovefromCart

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,40 +1,46 @@
 import React from 'react';
-import {FaWindowClose }from 'react-icons/fa';
-import { useSelector } from 'react-redux';
-// const items = useSelector(state => state.cart.items);
-const Cart = ({toggle,settoggle}) => {
-const closecart=()=>
-{
-settoggle(false)
-}  
-const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
-console.log(cartItems)
-return (
+import { FaWindowClose } from 'react-icons/fa';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeFromCart } from '../store/Slices/CartSlice';
+
+const Cart = ({ toggle, settoggle }) => {
+  const dispatch = useDispatch();
+  const closecart = () => {
+    settoggle(false)
+  }
+
+  const cartItems = useSelector(state => state.cart.items);
+
+  const removeItem = (id) => {
+    dispatch(removeFromCart({ id }));
+  }
+
+  return (
     <>
-    <div class="cart ">
-    <div class="cart__header">
-      <h2>My Cart</h2>
-      <button onClick={closecart}><FaWindowClose/></button>
-    </div>
-    <div class="cart__items">
-    {cartItems.map((item) => (
+      <div class="cart ">
+        <div class="cart__header">
+          <h2>My Cart</h2>
+          <button onClick={closecart}><FaWindowClose /></button>
+        </div>
+        <div class="cart__items">
+          {cartItems.map((item) => (
             <div className="cart__item" key={item.id}>
               <img src={item.image} alt="Product Image" />
               <div className="cart__item-details">
                 <h3>{item.title}</h3>
                 <p>Price: {item.price}</p>
-                <button className="cart__remove-btn">Remove</button>
+                <button className="cart__remove-btn" onClick={() => removeItem(item.id)}>Remove</button>
               </div>
             </div>
           ))}
-    </div>
-    <div class="cart__subtotal">
-      <p>Subtotal: $44.98</p>
-      <button class="cart__checkout-btn">Checkout</button>
-    </div>
-  </div>
+        </div>
+        <div class="cart__subtotal">
+          <p>Subtotal: $44.98</p>
+          <button class="cart__checkout-btn">Checkout</button>
+        </div>
+      </div>
     </>
   )
 }
 
-export default Cart
+export default Cart;
--- store/Slices/CartSlice.js
+++ store/Slices/CartSlice.js
@@ -6,21 +6,25 @@ export const CartSlice = createSlice({
     items:[],
   },
   reducers: {
-addtocart:(state,action)=>{
-    const { id, title, price,image } = action.payload;
-    const existingItem = state.items.find(item => item.id === id);
-    if (existingItem) {
+    addtocart: (state, action) => {
+      const { id, title, price, image } = action.payload;
+      const existingItem = state.items.find(item => item.id === id);
+      if (existingItem) {
         existingItem.quantity++;
       } else {
-        state.items.push({ id, title, price,image, quantity: 1 });
+        state.items.push({ id, title, price, image, quantity: 1 });
       }
       localStorage.setItem('cartItems', JSON.stringify(state.items));
     },
+    removeFromCart: (state, action) => {
+      const { id } = action.payload;
+      state.items = state.items.filter(item => item.id !== id);
+      localStorage.setItem('cartItems', JSON.stringify(state.items));
+    }
   },
 })
-// console.log(JSON.parse(localStorage.getItem('cartItems')))
 
 // Action creators are generated for each case reducer function
-export const { addtocart} = CartSlice.actions;
+export const { addtocart, removeFromCart } = CartSlice.actions;
 
 export default CartSlice.reducer;
